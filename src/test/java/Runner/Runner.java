a18f24ad360aab86b3d419b91daff0e82fafd8a9
BhatkarSameer <bhatkarsam1993@gmail.com>
2020-06-21 19:06:36 +0530

    Initial Commit from Local Master branch

---
+++ src/test/java/Runner/Runner.java
@@ -0,0 +1,18 @@
+package Runner;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src\\test\\java\\Features",
+        glue="StepDefinations"
+
+)
+
+public class Runner {
+
+    //Runner Main
+
+}
