3c3ba18d6747fd87e1aa0efbfa376125613a98e2
BhatkarSameer <bhatkarsam1993@gmail.com>
2020-06-28 11:23:29 +0530

    Added Collections.java with HashMap examples

--- src/main/java/Collections.java
+++ src/main/java/Collections.java
@@ -2,7 +2,9 @@ import java.util.*;
 
 public class Collections {
 
-    //Using HashSets
+    /*HashSet is an implementation of Set in which the elements are not sorted or ordered.
+    It is faster than a TreeSet.
+    The HashSet is an implementation of a Set.*/
     public void HashSets() {
 
         HashSet<String> strHashSet = new HashSet<String>();
@@ -12,7 +14,9 @@ public class Collections {
         System.out.println(strHashSet);
     }
 
-    //Using Sets
+    /*Set is a generic set of values with no duplicate elements.
+    A TreeSet is a set where the elements are sorted
+    Set is a parent interface of all set classes like TreeSet, HashSet, etc.*/
     public void Sets() {
 
         //'Set' is abstract and needs to be indirectly instantiated
@@ -38,20 +42,32 @@ public class Collections {
         }
     }
 
-    //Using HashMaps
+    //Using HashMaps when you want to be secure about the data exposure
+    //HashMaps are saved in <key><key_refrences_to_data> format
     public void HashMaps(){
 
-        //'Map' is abstract and needs to be indirectly instantiated
-        Map<String,String> strMap = new HashMap<String,String>();
-        strMap.put("This is object key","This is object value");
+        //'Map' is abstract and needs to be indirectly instantiated using HashMap
+        //Map can contain Lists and other Maps as a parameter
+        Map<String,List<String>> strMap = new HashMap<String,List<String>>();
+
+        //Implementing a List to be added to the Map
+        List<String> list_Emails = new ArrayList<String>();
+        list_Emails.add("email.email1@email.com");
+        list_Emails.add("email.email2@email.com");
+        list_Emails.add("email.email3@email.com");
+        list_Emails.add("email.email4@email.com");
+        list_Emails.add("email.email5@email.com");
+
+        //Adding the List to Map
+        strMap.put("Emails",list_Emails);
 
         //Print all Map contents
         System.out.println(strMap);
 
-        //Print specific value from Map
-        System.out.println(strMap.get("This is object key"));
-
-
+        //Print key > value representation from Map
+        Set<String> keys_HashMap = strMap.keySet();
+        for (String key : keys_HashMap){
+            System.out.println("Key from the HashMap >> " +key+ " represents value " + strMap.get(key));
+        }
     }
-
 }
