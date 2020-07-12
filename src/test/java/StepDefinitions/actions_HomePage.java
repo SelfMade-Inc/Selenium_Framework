7a7ab0ee862eb0305082c2c8885a738ce8d99e15
BhatkarSameer <bhatkarsam1993@gmail.com>
2020-07-12 11:38:49 +0530

    added webDefinitions *methods to work on frames *methods to work on javascript elements *methods to perform actions

---
+++ src/test/java/StepDefinitions/actions_HomePage.java
@@ -0,0 +1,44 @@
+package StepDefinitions;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class actions_HomePage {
+
+    @Given("User navigates to Webpage")
+    public void user_navigates_to_Webpage() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("User validates the Webpage title")
+    public void user_validates_the_Webpage_title() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("User navigates the webpage")
+    public void user_navigates_the_webpage() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("User reaches the desired screen successfully")
+    public void user_reaches_the_desired_screen_successfully() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("User verifies the screen is correct")
+    public void user_verifies_the_screen_is_correct() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("User can perform actions")
+    public void user_can_perform_actions() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
--- src/test/java/StepDefinitions/webDefinitions.java
+++ src/test/java/StepDefinitions/webDefinitions.java
@@ -1,11 +1,8 @@
 package StepDefinitions;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.*;
-import utilities.adv_initWebDriver;
 
 import java.time.Duration;
 import java.util.ArrayList;
@@ -14,13 +11,19 @@ import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+import utilities.adv_initWebDriver;
+
 public class webDefinitions extends adv_initWebDriver {
 
+    public static Actions action;
+    public static Alert alert;
     public static WebDriverWait waits;
     public static WebElement block;
-    List<String> list_value;
-    List<WebElement> list_element;
-    List<WebElement> href_links;
+    public static WebElement element;
+
+    public static List<String> list_value;
+    public static List<WebElement> list_element;
+    public static List<WebElement> href_links;
 
     String str = "";
 
@@ -47,27 +50,39 @@ public class webDefinitions extends adv_initWebDriver {
 
         if (size == 0) {
             return false;
-        } else return true;
+        } else
+            log.debug ("Webelements found");
+            return true;
+    }
+
+    //Frames
+    public static void workwithFrames(String frame_locator, String element_locator){
+        driver.switchTo ().frame (frame_locator);
+        list_element = driver.findElements (By.id (frame_locator));
+
+        for (WebElement element : list_element) {
+            isElementPresent ((By) driver.findElement (By.id (element_locator)));
+            log.debug ("Frame embedded element "+ element_locator + " found");
+        }
     }
 
     /* Overloading method waits to handle Implicit, Explicit and Fluent waits */
 
     //Call waits with number of seconds to implicitly wait
-    public void waits(Integer wait_time) {
+    public static void waits(Integer wait_time) {
         driver.manage ().timeouts ().implicitlyWait (wait_time, TimeUnit.SECONDS);
-        log.info ("WebDriver implicitly waits for " + wait_time + " seconds");
+        log.debug ("WebDriver implicitly waits for " + wait_time + " seconds");
     }
 
     //Call waits with Element locator and number of seconds to explicitly wait
-    public void waits(WebElement elemLocator, Integer wait_time) {
-
+    public static void waits(WebElement elemLocator, Integer wait_time) {
         waits = new WebDriverWait (driver, wait_time);
         waits.until (ExpectedConditions.presenceOfElementLocated ((By) elemLocator));
-        log.info ("WebDriver explicitly waits for " + wait_time + " seconds");
+        log.debug ("WebDriver explicitly waits for " + wait_time + " seconds");
     }
 
     //Call waits with 2 int and 2 string to fluently wait
-    public void waits(Integer wait_time, Integer poll_time, String fail_message, WebElement elemLocator) {
+    public static void waits(Integer wait_time, Integer poll_time, String fail_message, WebElement elemLocator) {
         Wait wait = new FluentWait (driver).withTimeout (Duration.ofSeconds (wait_time)).pollingEvery (Duration.ofSeconds (poll_time)).withMessage (fail_message).ignoring (NoSuchElementException.class);
 
         elemLocator = (WebElement) wait.until (new Function<WebDriver, WebElement> () {
@@ -75,30 +90,13 @@ public class webDefinitions extends adv_initWebDriver {
                 return driver.findElement (By.xpath ("elemLocator"));
             }
         });
-        log.info ("WebDriver fluently waits for " + wait_time + " seconds");
+        log.debug ("WebDriver fluently waits for " + wait_time + " seconds");
     }
 
-    /* Methods to work on WebElements */
-
-    //Reading and performing actions on dropdowns
-    public List<WebElement> handleDropdowns() {
-
-        //Locates the dropdown
-        WebElement dropdown = driver.findElement (By.tagName ("option"));
-
-        //Select class is the handling class for any kind of dropdowns
-        Select select = new Select (dropdown);
-        //Locates the option values from dropdown
-        list_element = driver.findElements (By.tagName ("option"));
-
-        log.info ("Values from dropdown are stored as list of webelements with size of" + list_element.size ());
-        //list_element = select.getOptions ();
-
-        return list_element;
-    }
+    /* Methods to read and store on WebElements */
 
     //Read text from dropdowns
-    public void readDropdowns() {
+    public static void readDropdowns() {
         list_element = handleDropdowns ();
         list_value = new ArrayList<String> ();
 
@@ -113,11 +111,11 @@ public class webDefinitions extends adv_initWebDriver {
         for (WebElement element : list_element) {
             list_value = list_element.stream ().map (WebElement::getText).collect (Collectors.toList ());
         }
-        log.info ("Values from dropdown are stored as list of strings with size of" + list_value.size ());
+        log.debug ("Values from dropdown are stored as list of strings with size of" + list_value.size ());
     }
 
     //Reading from links on a webpage
-    public List<WebElement> handleHREFS(By by) {
+    public static List<WebElement> handleHREFS(By by) {
 
         //Helps filter which links to work on by assigning div block as a Webelement
         block = driver.findElement (by);
@@ -125,7 +123,7 @@ public class webDefinitions extends adv_initWebDriver {
         //Locates all links from the div block
         href_links = block.findElements (By.tagName ("a"));
 
-        log.info ("Values from total links on webpage are stored as list of webelements with size of" + href_links.size ());
+        log.debug ("Values from total links on webpage are stored as list of webelements with size of" + href_links.size ());
 
         for (WebElement href : href_links) {
             //Write code to specify action on links e.g.Click or getAttribute.value
@@ -134,8 +132,8 @@ public class webDefinitions extends adv_initWebDriver {
         return href_links;
     }
 
-    //Datatables
-    public void handleDatatables(String locate_rows, String locate_cols, By by){
+    //DataTables
+    public static void handleDatatables(String locate_rows, String locate_cols, By by){
 
         List<WebElement> list_datatable_rows = driver.findElements (By.xpath (locate_rows));
         List<WebElement> list_datatable_cols = driver.findElements (By.xpath (locate_cols));
@@ -145,5 +143,82 @@ public class webDefinitions extends adv_initWebDriver {
                 //Write code to specify action on Datatable
             }
         }
+        log.debug (" DataTable dimensions are" +list_datatable_rows.size ()+ " by " +list_datatable_cols.size () );
+    }
+
+    /* Actions to be performed on WebElements */
+
+    //Mouse Click on webelements
+    public static void mouseClick(By by){
+        waits(10);
+        //driver.findElement (by).click ();
+        action.moveToElement (driver.findElement (by)).perform ();
+    }
+
+    //Right click on an element
+    public static void right_mouseClick(By by){
+        element = driver.findElement (by);
+        action.contextClick (element).perform ();
+    }
+
+    //Drag elements like sliders
+    public static void dragNdrop(By by){
+        element = driver.findElement (by);
+        //if Element is slider
+        int width = element.getSize ().width;
+        int height = element.getSize ().height;
+
+        if(height==0) {
+            //To drag slider on X axis
+            action.dragAndDropBy (element, width, 0);
+        }
+        else if(width==0){
+            //To drag slider on Y axis
+            action.dragAndDropBy (element, 0, height);
+        }
+        log.debug ("Slider drag performed");
+    }
+
+    //Drag elements and drop at destination elements
+    public static void dragNdrop(By by_source, By by_destination){
+        WebElement source_element = driver.findElement (by_source);
+        WebElement destination_element = driver.findElement (by_destination);
+
+        action.dragAndDrop (source_element,destination_element).perform ();
+        log.debug ("Drag and drop performed");
+    }
+
+    //Reading and performing actions on dropdowns
+    public static List<WebElement> handleDropdowns() {
+
+        //Locates the dropdown
+        WebElement dropdown = driver.findElement (By.tagName ("option"));
+
+        //Select class is the handling class for any kind of dropdowns
+        Select select = new Select (dropdown);
+        //Locates the option values from dropdown
+        list_element = driver.findElements (By.tagName ("option"));
+
+        log.debug ("Values from dropdown are stored as list of webelements with size of" + list_element.size ());
+        //list_element = select.getOptions ();
+
+        return list_element;
+    }
+
+    //JavaScript alerts
+    public static void handleAlerts(){
+
+        alert = driver.switchTo ().alert ();
+        String alert_message = alert.getText ();
+        alert.accept ();
+        log.debug (alert_message+ " Alert received and accepted");
+    }
+
+    public static void handleAlerts(Integer wait_time){
+        waits ( wait_time);
+        alert = waits.until (ExpectedConditions.alertIsPresent ());
+        String alert_message = alert.getText ();
+        alert.accept ();
+        log.debug (alert_message+ " Alert received and accepted");
     }
 }
