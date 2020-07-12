7ed189369d01be26a3a2fe9df94f2a0cc365ce9e
BhatkarSameer <bhatkarsam1993@gmail.com>
2020-07-12 12:17:12 +0530

    added webDefinitions *methods to work using JAVAScript Executor

--- src/test/java/StepDefinitions/webDefinitions.java
+++ src/test/java/StepDefinitions/webDefinitions.java
@@ -3,20 +3,22 @@ package StepDefinitions;
 import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.*;
+import utilities.adv_initWebDriver;
 
 import java.time.Duration;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 import java.util.stream.Collectors;
-
-import utilities.adv_initWebDriver;
 
 public class webDefinitions extends adv_initWebDriver {
 
     public static Actions action;
     public static Alert alert;
+    public static JavascriptExecutor js = (JavascriptExecutor) driver;
     public static WebDriverWait waits;
     public static WebElement block;
     public static WebElement element;
@@ -52,18 +54,40 @@ public class webDefinitions extends adv_initWebDriver {
             return false;
         } else
             log.debug ("Webelements found");
-            return true;
+        return true;
     }
 
     //Frames
-    public static void workwithFrames(String frame_locator, String element_locator){
+    public static void workwithFrames(String frame_locator, String element_locator) {
         driver.switchTo ().frame (frame_locator);
         list_element = driver.findElements (By.id (frame_locator));
 
         for (WebElement element : list_element) {
             isElementPresent ((By) driver.findElement (By.id (element_locator)));
-            log.debug ("Frame embedded element "+ element_locator + " found");
+            log.debug ("Frame embedded element " + element_locator + " found");
         }
+    }
+
+    //Manage and work with multiple Window handles
+    public static void managemultipleWindows() {
+        Set<String> windowHandles = driver.getWindowHandles ();
+        Iterator<String> iterator = windowHandles.iterator ();
+
+        while (iterator.hasNext ()) {
+            iterator.next ();
+            //method action call
+        }
+        log.debug ("Total window handles available is " + windowHandles.size ());
+    }
+
+    /* Working with JAVAScript executor */
+
+    //Highlighting elements to focus
+    public static void setHighlighter(By by) {
+        element = driver.findElement (by);
+        js.executeScript ("arguments[0].style.border='3px solid red'", element);
+        waits (5);
+        //Call method action
     }
 
     /* Overloading method waits to handle Implicit, Explicit and Fluent waits */
@@ -133,46 +157,45 @@ public class webDefinitions extends adv_initWebDriver {
     }
 
     //DataTables
-    public static void handleDatatables(String locate_rows, String locate_cols, By by){
+    public static void handleDatatables(String locate_rows, String locate_cols, By by) {
 
         List<WebElement> list_datatable_rows = driver.findElements (By.xpath (locate_rows));
         List<WebElement> list_datatable_cols = driver.findElements (By.xpath (locate_cols));
 
-        for(WebElement element_row : list_datatable_rows){
-            for(WebElement element_col : list_datatable_cols){
+        for (WebElement element_row : list_datatable_rows) {
+            for (WebElement element_col : list_datatable_cols) {
                 //Write code to specify action on Datatable
             }
         }
-        log.debug (" DataTable dimensions are" +list_datatable_rows.size ()+ " by " +list_datatable_cols.size () );
+        log.debug (" DataTable dimensions are" + list_datatable_rows.size () + " by " + list_datatable_cols.size ());
     }
 
     /* Actions to be performed on WebElements */
 
     //Mouse Click on webelements
-    public static void mouseClick(By by){
-        waits(10);
+    public static void mouseClick(By by) {
+        waits (10);
         //driver.findElement (by).click ();
         action.moveToElement (driver.findElement (by)).perform ();
     }
 
     //Right click on an element
-    public static void right_mouseClick(By by){
+    public static void right_mouseClick(By by) {
         element = driver.findElement (by);
         action.contextClick (element).perform ();
     }
 
     //Drag elements like sliders
-    public static void dragNdrop(By by){
+    public static void dragNdrop(By by) {
         element = driver.findElement (by);
         //if Element is slider
         int width = element.getSize ().width;
         int height = element.getSize ().height;
 
-        if(height==0) {
+        if (height == 0) {
             //To drag slider on X axis
             action.dragAndDropBy (element, width, 0);
-        }
-        else if(width==0){
+        } else if (width == 0) {
             //To drag slider on Y axis
             action.dragAndDropBy (element, 0, height);
         }
@@ -180,11 +203,11 @@ public class webDefinitions extends adv_initWebDriver {
     }
 
     //Drag elements and drop at destination elements
-    public static void dragNdrop(By by_source, By by_destination){
+    public static void dragNdrop(By by_source, By by_destination) {
         WebElement source_element = driver.findElement (by_source);
         WebElement destination_element = driver.findElement (by_destination);
 
-        action.dragAndDrop (source_element,destination_element).perform ();
+        action.dragAndDrop (source_element, destination_element).perform ();
         log.debug ("Drag and drop performed");
     }
 
@@ -206,19 +229,19 @@ public class webDefinitions extends adv_initWebDriver {
     }
 
     //JavaScript alerts
-    public static void handleAlerts(){
+    public static void handleAlerts() {
 
         alert = driver.switchTo ().alert ();
         String alert_message = alert.getText ();
         alert.accept ();
-        log.debug (alert_message+ " Alert received and accepted");
+        log.debug (alert_message + " Alert received and accepted");
     }
 
-    public static void handleAlerts(Integer wait_time){
-        waits ( wait_time);
+    public static void handleAlerts(Integer wait_time) {
+        waits (wait_time);
         alert = waits.until (ExpectedConditions.alertIsPresent ());
         String alert_message = alert.getText ();
         alert.accept ();
-        log.debug (alert_message+ " Alert received and accepted");
+        log.debug (alert_message + " Alert received and accepted");
     }
 }
